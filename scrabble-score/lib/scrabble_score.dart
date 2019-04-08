4706824551d8f45bd269ca4221961bb60529dab2
bondhan <bondhan@texo.id>
2019-04-08 13:59:45 +0700

    done scrabble challenge

--- scrabble-score/lib/scrabble_score.dart
+++ scrabble-score/lib/scrabble_score.dart
@@ -1,1 +1,18 @@
+
+
 // Put your code here
+int score(String words){
+
+  var letters = ["","AEIOULNRST", "DG", "BCMP", "FHVWY", "K", "","","JX","", "QZ"];
+  int total = 0;  
+  for (int i=0; i<words.length; i++) {
+    for (int k=0; k<letters.length; k++){
+      if (letters[k].contains(words[i].toUpperCase())){
+        total += k;
+        break;
+      }
+    }
+  }
+
+  return total;  
+}
