263b23209a9ec1505edf6ff00f967ec2aed4eaf8
bondhan <bondhan@texo.id>
2019-04-08 14:09:50 +0700

    done reverse string

--- reverse-string/lib/reverse_string.dart
+++ reverse-string/lib/reverse_string.dart
@@ -1,3 +1,9 @@
-String reverse() {
-  // Put your code here
+String reverse(String word) {
+
+  String reverse = "";
+  for (int i=word.length-1;i>=0;i--){
+    reverse += word[i];
+  }
+
+  return reverse;
 }
